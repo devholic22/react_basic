6f43e00ee1d7d0e4d19d8694505cc13d17e31cab
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-06 15:36:16 +0900

    7.1 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -24,6 +24,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((element, index) => (
+          <li key={index}>{element}</li>
+        ))}
+      </ul>
     </div>
   );
 }
